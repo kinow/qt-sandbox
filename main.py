315a83c176686fe40854e27f6c7c76939c02de9a
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2017-01-04 22:35:34 +1300

    Display context menu

--- main.py
+++ main.py
@@ -3,8 +3,34 @@
 # NOTE: needs Python 2, for pyqt5
 
 import sys
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import (QWidget, QLineEdit, QLabel, QPushButton, 
-    QHBoxLayout, QVBoxLayout, QApplication)
+    QHBoxLayout, QVBoxLayout, QMenu, QWidgetAction, QApplication)
+
+class PopupAction(QWidgetAction):
+
+    def __init__(self, parent):
+        super(QWidgetAction, self).__init__(parent)
+        widget = QWidget()
+        label1 = QLabel()
+        input1 = QLineEdit()
+        hbox = QHBoxLayout()
+        hbox.addWidget(label1)
+        hbox.addWidget(input1)
+        widget.setLayout(hbox)
+        self.setDefaultWidget(widget)
+
+class PopupMenu(QMenu):
+
+    def __init__(self):
+        super(QMenu, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.addAction(PopupAction(self))
+
+    def display(self):
+        print("Displaying....")
 
 class Window(QWidget):
 
@@ -15,7 +41,13 @@ class Window(QWidget):
     def initUI(self):
         label1 = QLabel()
         label1.setText("Enter your text")
+
         input1 = QLineEdit()
+
+        button1 = QPushButton("Show menu")
+
+        popup = PopupMenu()
+        button1.setMenu(popup)
 
         hbox = QHBoxLayout()
         hbox.addWidget(label1)
@@ -23,13 +55,18 @@ class Window(QWidget):
 
         vbox = QVBoxLayout()
         vbox.addLayout(hbox)
-        
+        vbox.addWidget(button1)
         
         self.setLayout(vbox)
 
         self.setGeometry(300, 300, 300, 150)
         self.setWindowTitle('Buttons')    
         self.show()
+
+    def addInputBox(self):
+        print("OI")
+        pass
+
 
 if __name__ == '__main__':
     
