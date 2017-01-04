f7ca51bc5e41faa7e03ac080a68b61a3f0c24523
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2017-01-04 21:57:08 +1300

    Adding mor elements

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 # NOTE: needs Python 2, for pyqt5
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit
+from PyQt5.QtWidgets import (QWidget, QLineEdit, QLabel, QPushButton, 
+    QHBoxLayout, QVBoxLayout, QApplication)
 
 class Window(QWidget):
 
@@ -12,8 +13,22 @@ class Window(QWidget):
         self.initUI()
 
     def initUI(self):
+        label1 = QLabel()
+        label1.setText("Enter your text")
         input1 = QLineEdit()
 
+        hbox = QHBoxLayout()
+        hbox.addWidget(label1)
+        hbox.addWidget(input1)
+
+        vbox = QVBoxLayout()
+        vbox.addLayout(hbox)
+        
+        
+        self.setLayout(vbox)
+
+        self.setGeometry(300, 300, 300, 150)
+        self.setWindowTitle('Buttons')    
         self.show()
 
 if __name__ == '__main__':
@@ -21,7 +36,7 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     w = Window()
-    w.resize(250, 150)
+    w.resize(530, 140)
     w.move(300, 300)
     w.setWindowTitle('Simple')
     w.show()
